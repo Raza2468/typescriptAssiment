e1f081532d73270e2fab101d11e1a5f5827f6694
Raza <71090111+Raza2468@users.noreply.github.com>
2023-01-02 21:19:42 +0500

    task currency convert complete

---
+++ Currency Conveter/dist/index.js
@@ -0,0 +1,62 @@
+import inquirer from "inquirer";
+let runStatment = true;
+while (runStatment) {
+    let currency = await inquirer.prompt([
+        {
+            type: "list",
+            name: "currencyArr",
+            choices: ["PKR", "USD", "EURO"],
+            message: "Select the current Convert Currency",
+        },
+        {
+            type: "input",
+            name: "EnterAmount",
+            message: "",
+        },
+        {
+            type: "list",
+            name: "convertCurrencyArr",
+            choices: ["PKR", "USD", "EURO"],
+            message: "Select the Convert Currency",
+        },
+    ]);
+    let { currencyArr, EnterAmount, convertCurrencyArr } = currency;
+    if (EnterAmount) {
+        switch (currencyArr) {
+            case "PKR" || "USD":
+                console.log("PKR Convert to USD", EnterAmount * 0.0044);
+                break;
+            case "PKR" || "EURO":
+                console.log("PKR Convert to EURO", EnterAmount * 241);
+                break;
+            case "USD" || "PKR":
+                console.log("USD Convert to PKR", EnterAmount * 226);
+                break;
+            case "USD" || "EURO":
+                console.log("USD Convert to EURO", EnterAmount * 241);
+                break;
+            case "EURO" || "PKR":
+                console.log("USD Convert to PKR", EnterAmount * 0.0044);
+                break;
+            case "EURO" || "USD":
+                console.log("USD Convert to PKR", EnterAmount * 226);
+                break;
+            default:
+                console.log("Please inter");
+                break;
+        }
+        let reltime = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "reltimeConform",
+                message: "agin use in convert Currency",
+            }
+        ]);
+        let { reltimeConform } = reltime;
+        runStatment = reltimeConform;
+    }
+    else {
+        console.log("Please inter");
+    }
+}
+//# sourceMappingURL=index.js.map
